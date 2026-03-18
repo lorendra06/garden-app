c3b8e650e89e10f943c48ab2e09c18765dc10bd7
Loredana <lorendra06@gmail.com>
2026-03-18 17:42:02 -0500

    Refactor season and advice logic into functions (closes #1)

--- garden_advice.py
+++ garden_advice.py
@@ -1,30 +1,58 @@
-# garden_advice.py
-# An app that provides gardening tips based on the current month and season.
+"""
+garden_advice.py
+An app that provides gardening tips based on the current month
+and season. Provides advice based on the season determined
+from the given month.
+TODO: Replace hardcoded month number with dynamic input from the user
+"""
 
-# TODO: Replace hardcoded month number with dynamic input from the user
 month = 3
 
-# TODO: Refactor the season logic into a separate function called get_season(month)
-if month in [12, 1, 2]:
-    season = "Winter"
-elif month in [3, 4, 5]:
-    season = "Spring"
-elif month in [6, 7, 8]:
-    season = "Summer"
-else:
-    season = "Autumn"
 
-# TODO: Refactor the advice logic into a separate function called get_advice(season)
-if season == "Winter":
-    advice = "Protect your plants from frost. Consider indoor gardening."
-elif season == "Spring":
-    advice = "Great time to plant seeds! Make sure to water regularly."
-elif season == "Summer":
-    advice = "Water your plants early in the morning to avoid evaporation."
-else:
-    advice = "Harvest remaining crops and prepare soil for winter."
+def get_season(month):
+    """
+    Return the season name based on the given month number.
+    """
+    if month in [12, 1, 2]:
+        return "Winter"
+    elif month in [3, 4, 5]:
+        return "Spring"
+    elif month in [6, 7, 8]:
+        return "Summer"
+    else:
+        return "Autumn"
 
-# TODO: Add a docstring to document what this program does
+
+def get_advice(season):
+    """
+    Return gardening advice based on the given season.
+    """
+    if season == "Winter":
+        advice = (
+            "Protect your plants from frost. "
+            "Consider indoor gardening."
+        )
+    elif season == "Spring":
+        advice = (
+            "Great time to plant seeds! "
+            "Make sure to water regularly."
+        )
+    elif season == "Summer":
+        advice = (
+            "Water your plants early in the morning "
+            "to avoid evaporation."
+        )
+    else:
+        advice = (
+            "Harvest remaining crops and "
+            "prepare soil for winter."
+        )
+    return advice
+
+
+season = get_season(month)
+advice = get_advice(season)
+
 print(f"Month: {month}")
 print(f"Season: {season}")
 print(f"Gardening tip: {advice}")
